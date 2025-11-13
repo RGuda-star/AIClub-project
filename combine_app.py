c3e12e0148ea4ab04b6e17ac0639d55044458460
RGuda-star <gudarish@msu.edu>
2025-11-12 20:48:50 -0500

    update

--- combine_app.py
+++ combine_app.py
@@ -59,12 +59,14 @@ def get_nlp_models():
     # Load spaCy model (disable unused components for speed). This will be used to tokenize our reviews
 
     try:
-        nlp = spacy.load(str(Path(__file__).parent / "model" / "en_core_web_sm"), disable=["parser", "ner"])
+        nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
 
     except OSError:
         from spacy.cli import download
         download("en_core_web_sm")
-        nlp = spacy.load(str(Path(__file__).parent / "model" / "en_core_web_sm"), disable=["parser", "ner"])
+        nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
 
 
 
